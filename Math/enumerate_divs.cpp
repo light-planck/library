ded276893a3ea8b57eda4bf44d6536e91d6e4432
Planck16 <Planck16@users.noreply.github.com>
2022-04-25 11:26:38 +0900

    関数名をcaldivからenumerate_divsにを変更し、llをlong longに変更

--- Math/enumerate_divs.cpp
+++ Math/enumerate_divs.cpp
@@ -1,24 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define all(x) (x).begin(),(x).end()
-using ll = long long;
+
 
 // 約数列挙
-vector<ll> caldiv(ll n) {
-  vector<ll> div;
-  for (ll i = 1; i * i <= n; i++) {
+vector<long long> enumerate_divs(long long n) {
+  vector<long long> divs;
+
+  for (long long i = 1; i * i <= n; i++) {
     if (n % i != 0) continue;
-    div.push_back(i);
+    div.emplace_back(i);
 
     // n = i * iの場合を除いて、もう一方の約数を追加
     if (n / i != i) div.push_back(n / i);
   }
-  sort(all(div));
-  return div;
+  sort(divs.begin(), divs.end());
+
+  return divs;
 }
+
 
 int main() {
   int n = 36;
-  for (auto x : caldiv(n)) cout << x << endl;
+  for (auto x : enumerate_divs(n)) cout << x << endl;
   return 0;
 }
