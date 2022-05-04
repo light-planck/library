41159e72c75095534ce541db2b065cd7c536ade2
Planck16 <Planck16@users.noreply.github.com>
2022-05-04 17:22:08 +0900

    fixed isprime

--- number/isprime.cpp
+++ number/isprime.cpp
@@ -1,24 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
-using ll = long long;
+
 
 // 2, 3, ... , sqrt(n)まで調べればよい
+bool is_prime(long long x) {
+  if (x < 2) return false;
 
-bool IsPrime(long long n) {
-  if (n <= 1) return false;
-  if (n == 2) return true;
-
-  for (long long i = 3; i * i <= n; i += 2) {
-    if (n % i == 0) return false;
+  for (long long i = 2; i * i <= x; ++i) {
+    if (x % i == 0) return false;
   }
 
   return true;
-}
-
-int main() {
-  int a = 9;
-  int b = 5;
-  if (isprime(b)) cout << b << " is prime" << endl;
-  else cout << a << " is not prime" << endl;
-  return 0;
 }
